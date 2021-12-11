c32ac02a1b5b410ab2415f9c9f3c74950fec4eaa
Gorin <artemkscv@gmail.com>
2021-12-11 19:08:28 +0300

    asd

--- client/script.js
+++ client/script.js
@@ -1,9 +1,11 @@
-$("#b").on("click", function(){
-    console.log(777);
-});
 var request = $.get("http://localhost:591/quote");
 request.done(function(data){
-    console.log(data);
+    var dict_quote = JSON.parse(data);
+    var title = "Цитата" + dict_quote["number"];
+    var content = dict_quote["quote"];
+    $("title").text(title);
+    $("#text").text(content);
+    console.log(dict_quote);
 });
 request.fail(function(jkXHR, textStatus, errorThrown){
     console.log(textStatus, errorThrown);
--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 var express = require("express");
 var cors = require("cors");
+const { randomInt } = require("crypto");
 var app = express();
 app.use(cors());
-app.get("/quote", function(request, response){
-    response.send("000");
-});
-app.listen(591);
+app.get("/quote" ,  function(request, response) { 
+    var fs = require("fs");
+    var fileContent = fs.readFileSync("server/db.txt", "utf-8");
+    var strings = fileContent.split("\n");
+    var randomNum =  randomInt(strings.length);
+    var dict_quote = {
+        number: (randomNum + 1),
+        quote: strings[randomNum] 
+    }
+    var data = JSON.stringify(dict_quote)
+    response.send(data);
+}); 
+app.listen (591);
